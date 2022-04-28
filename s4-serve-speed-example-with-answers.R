0f5e45e54422c24047bdba491d09957b853f138e
adrien ickowicz <ickowism@gmail.com>
2022-04-28 16:19:52 +1000

    s4 final

--- s4-serve-speed-example-with-answers.R
+++ s4-serve-speed-example-with-answers.R
@@ -17,15 +17,6 @@ ggplot(data = dat, aes(x = err_rate, y = bp_rate) )+ geom_point() + geom_smooth(
 
 # Data kindly provided by Felipe Lima (Ori)
 
-d <- dir("example_data/Serve_Speed_Example/", full.names = TRUE)
-xl <- lapply(d, dv_read, skill_evaluation_decode = "guess")
-x <- bind_rows(lapply(xl, plays))
-sx <- x %>% filter(skill == "Serve") %>% mutate(serve_speed = as.numeric(custom_code))
-sx$skill_type[sx$skill_type == "Float serve"] <- "Jump-float serve"
-sx <- sx %>% select(match_id, point_id, set_number, team, player_number, player_id, skill, skill_type, evaluation, evaluation_code, point_won_by, serve_speed)
-
-saveRDS(sx, file = "example_data/Serve_Speed_Example/serve_speed_example.rds")
-
 sx <- readRDS(file = "example_data/Serve_Speed_Example/serve_speed_example.rds")
 
 dat <- dplyr::filter(sx, skill_type == "Jump serve" & serve_speed >= 60) %>%
--- s4-serve-speed-example.R
+++ s4-serve-speed-example.R
@@ -2,4 +2,15 @@ library(dplyr)
 library(datavolley)
 library(mgcv)
 
+# Serve agressiveness
+
+sx <- readRDS("example_data/DE Men 2019/processed.rds")
+
+
+
+# Serve speed
+
+# Data kindly provided by Felipe Lima (Ori)
+
+
 sx <- readRDS(file = "example_data/Serve_Speed_Example/serve_speed_example.rds")
